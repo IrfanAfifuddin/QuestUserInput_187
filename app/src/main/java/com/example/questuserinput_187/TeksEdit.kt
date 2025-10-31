be37cb491ddea855dd154faea0fd4cec76a9c1d9
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-10-31 09:20:08 +0700

    make function FormDataDiri and add variabel

---
+++ app/src/main/java/com/example/questuserinput_187/TeksEdit.kt
@@ -0,0 +1,21 @@
+package com.example.questuserinput_187
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+}
